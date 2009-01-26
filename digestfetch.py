3f0e2e4a15dd62f2252e9617803031322b48785c
James Wheare <james@bogart.local>
2009-01-26 20:13:22 +0000

    Add TFL scraping tube status fetcher

--- digestfetch.py
+++ digestfetch.py
@@ -5,6 +5,13 @@ digestfetch.py
 Fetch bitesize content from the world of the web for use in a daily digest pocketmod
 """
 
+import re, sys, getopt, urllib, urllib2, httplib, socket
+from cgi import parse_qs
+from copy import copy
+
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+AMPERSAND_MASSAGE = copy(BeautifulSoup.MARKUP_MASSAGE)
+
 import pylast
 
 from settings import *
@@ -30,5 +37,35 @@ def lastfm_event_recommendations():
     events = user.getRecommendedEvents(limit=5)
     return events
 
+def tube_status():
+    """Fetch Tube status from TFL"""
+    url = "http://www.tfl.gov.uk/tfl/livetravelnews/realtime/tube/default.html"
+    soup = BeautifulSoup(urllib.urlopen(url), markupMassage=AMPERSAND_MASSAGE,
+        parseOnlyThese=SoupStrainer("div", { "id": "service-board" }))
+    
+    # Parse line status
+    lines = soup.find("dl", { "id": "lines" }).findAll("dt")
+    line_status = {}
+    for line in lines:
+        status = line.findNext("dd")
+        if status.h3:
+            line_status[line.string] = status.h3.string
+        else: 
+            line_status[line.string] = status.string
+    # Parse station status
+    station_categories = soup.find("dl", { "id": "stations" }).findAll("dt")
+    station_status = {}
+    for category in station_categories:
+        stations = []
+        next = category.findNextSibling(re.compile("dt|dd"))
+        while next and next.name != u"dt":
+            if next.h3:
+                stations.append(next.h3.string)
+            next = next.findNextSibling(re.compile("dt|dd"))
+        station_status[category.string] = stations
+    
+    return line_status, station_status
+
 if __name__ == '__main__':
-    lastfm_event_recommendations()
+    import pprint
+    pprint.pprint(tube_status())
