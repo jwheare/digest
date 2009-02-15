fa4523a209b5eb68f58289b8b91036e64bb33abc
jwheare <james@bogart.home>
2009-02-15 01:43:14 +0000

    Example settings file

---
+++ settings.example.py
@@ -0,0 +1,29 @@
+GMAPS_KEY = '[api key]'
+
+LASTFM_KEY = '[api key]'
+LASTFM_SECRET = '[api secret]'
+LASTFM_SESSION = '[permanent api session key]'
+
+TWITTER_USERNAME = '[username]'
+TWITTER_PASSWORD = '[password]'
+TWITTER_USERID = '[userid]'
+
+NEWSGATOR_KEY = '[api key]'
+NEWSGATOR_USERNAME = '[username]'
+NEWSGATOR_PASSWORD = '[password]'
+NEWSGATOR_LOCATIONID = '[location id]'
+
+GCAL_USERNAME = '[username]'
+GCAL_PASSWORD = '[password]'
+GCAL_CALENDARS = {
+    '[calendar id]': ('[display name]', '[hex colour]'),
+}
+
+# See http://www.bbc.co.uk/weather/5day.shtml?world=4581
+BBC_WEATHER_LOCATION = 4581
+# See http://www.metoffice.gov.uk/weather/uk/se/se_forecast_weather.html
+MET_WEATHER_REGION = 'se'
+MET_WEATHER_REGION_FULL = "London & South East England"
+
+FLICKR_KEY = '[api key]'
+FLICKR_SECRET = '[api secret]'
